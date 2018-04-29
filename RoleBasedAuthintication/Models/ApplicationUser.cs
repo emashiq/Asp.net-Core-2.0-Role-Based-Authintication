04b0bf112f68cfe5c7970001666240a66186a043
Ashiqur Rahman Emran <em.ashiq@gmail.com>
2018-04-29 15:14:47 +0600

    Add project files.

---
+++ RoleBasedAuthintication/Models/ApplicationUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RoleBasedAuthintication.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    [Table("UserAccounts")]
+    public class ApplicationUser : IdentityUser
+    {
+        public UserProfile UserProfile { get; set; }
+    }
+
+    public class UserProfile
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ImageUrl { get; set; }
+        public string FacebookUrl { get; set; }
+        public string GitHubUrl { get; set; }
+        public string LinkedinUrl { get; set; }
+        public string GooglePlusUrl { get; set; }
+        public string OtherUrl { get; set; }
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string ApplicationUserID { get; set; }
+        
+
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+    
+}
---
+++ RoleBasedAuthintication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RoleBasedAuthintication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
